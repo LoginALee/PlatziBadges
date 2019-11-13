f7492dc2253a00bbb2f5e00c07362f3b03fe441c
LoginALee <Alejandrodxwwe@hotmail.com>
2019-11-12 20:15:13 -0600

    Añadiendo Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -11,13 +11,13 @@ class Badge extends React.Component{
             </div>
 
             <div className='Badge__section-name'>
-                <img className='Badge__avatar' src="https://s.gravatar.com/avatar/8e51354a95ae7b18a324a03e171b7ddc?s=80" alt="Avatar"/>
-                <h1> Alejandro <br/> Tinoco</h1>
+                <img className='Badge__avatar' src={this.props.avatarUrl} alt="Avatar"/>
+                <h1> {this.props.firstName} <br/> {this.props.lastName}</h1>
             </div>
 
             <div className='Badge__section-info'>
-                <h3>Frontend Engineer</h3>
-                <div>@LoginALee</div>
+                <h3>{this.props.jobTitle}</h3>
+                <div>@{this.props.twitter}</div>
             </div>
 
             <div className='Badge__footer'> #Platzi</div>
--- src/index.js
+++ src/index.js
@@ -7,5 +7,12 @@ import './global.css';
 
 const container = document.getElementById('app');
 
-//reactDOM.render(__qué__, __dónde__);
-reactDOM.render(<Badge />, container);
+
+reactDOM.render(
+    <Badge 
+        firstName='Alejandro'
+        lastName='Tinoco'
+        jobTitle='Frontend Engineer'
+        twitter='LoginALee'
+        avatarUrl='https://s.gravatar.com/avatar/8e51354a95ae7b18a324a03e171b7ddc?s=80'
+    />, container);

59836723ad5d583a195f99c429df5fd6f43cf45d
LoginALee <Alejandrodxwwe@hotmail.com>
2019-11-18 17:04:55 -0600

    Lista de componentes

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,5 +1,7 @@
 import  React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import './styles/BadgesList.css';
 
 class BadgesList extends React.Component{
     render(){
@@ -7,8 +9,24 @@ class BadgesList extends React.Component{
             <ul className="list-unstyled">
                 {this.props.badges.map((badge) =>{
                     return(
-                        <li key={badge.id}>
-                            <p>{badge.firstName} {badge.lastName}</p>
+                        <li className='BadgesListItem' key={badge.id}>
+                        <div className='container'>
+                            <div className="row">
+                                <div className="col-2">
+                                    <img className="BadgesListItem__avatar" src={badge.avatarUrl} alt=""/>
+                                    
+                                </div>
+                                <div className="col-10">
+                                    <div className="ml-3">
+                                        <span className='font-weight-bold'>{badge.firstName} {badge.lastName}</span>
+                                        <br/>
+                                        <span className='twitterData'><FontAwesomeIcon icon= {faTwitter} /> @{badge.twitter}</span>
+                                        <br/>
+                                        <span>{badge.jobTitle}</span>
+                                    </div>
+                                </div>
+                            </div>  
+                        </div>
                         </li>
                     )
                 })}
